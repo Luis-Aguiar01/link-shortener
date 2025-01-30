7e43b529735688c6402527ac70bda86d2607f989
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-01-30 16:57:55 -0300

    Configuração inicial dos servlets front controller

--- src/main/java/br/edu/ifsp/dsw1/controller/DisconnectedServlet.java
+++ src/main/java/br/edu/ifsp/dsw1/controller/DisconnectedServlet.java
@@ -2,6 +2,9 @@ package br.edu.ifsp.dsw1.controller;
 
 import java.io.IOException;
 
+import br.edu.ifsp.dsw1.controller.command.Command;
+import br.edu.ifsp.dsw1.controller.command.ErrorCommand;
+import br.edu.ifsp.dsw1.controller.command.HomeCommand;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -13,7 +16,18 @@ public class DisconnectedServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	public void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		var action = request.getAttribute("action");
+		Command command = null;
 		
+		if ("home".equals(action)) {
+			command = new HomeCommand();
+		} else {
+			command = new ErrorCommand();
+		}
+	
+		var view = command.execute(request, response);
+		var dispatcher = request.getRequestDispatcher(view);
+		dispatcher.forward(request, response);
 	}
 	
 	@Override
