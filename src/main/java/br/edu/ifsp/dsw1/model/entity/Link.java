0223d17a6240cc9211e755a0247334377837d9c5
Matheus Mantovani Gonçalves <mantovanimatheus335@gmail.com>
2025-01-31 08:49:42 -0300

    Criação da entidade Link

---
+++ src/main/java/br/edu/ifsp/dsw1/model/entity/Link.java
@@ -0,0 +1,30 @@
+package br.edu.ifsp.dsw1.model.entity;
+
+public class Link {
+	private String customLink;
+	private String link;
+	
+	public Link() {
+		super();
+	}
+
+	public Link(String customLink, String link) {
+		super();
+		this.customLink = customLink;
+		this.link = link;
+	}
+	
+	public String getCustomLink() {
+		return customLink;
+	}
+	
+	public void setCustomLink(String customLink) {
+		this.customLink = customLink;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+}
