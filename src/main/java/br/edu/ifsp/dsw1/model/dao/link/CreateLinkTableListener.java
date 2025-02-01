ce02019d7284c9bf7b113677413d18244e754384
CristianoRDO <crisrooliveira.2004@gmail.com>
2025-02-01 14:26:18 -0300

    Implementação Inicial de Listener Para Criação da Tabela Link - Pendente Query SQL

--- src/main/java/br/edu/ifsp/dsw1/model/dao/link/CreateLinkTableListener.java
+++ src/main/java/br/edu/ifsp/dsw1/model/dao/link/CreateLinkTableListener.java
@@ -1,5 +1,21 @@
 package br.edu.ifsp.dsw1.model.dao.link;
 
-public class CreateLinkTableListener {
+import br.edu.ifsp.dsw1.model.dao.connection.DatabaseConnection;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+
+public class CreateLinkTableListener implements ServletContextListener  {
+	
+	private static final String CREATE_LINK_TABLE_SQL = "";
+	
+	public void contextInitialized(ServletContextEvent sce)  { 
+        try (var conn = DatabaseConnection.getConnection();
+       	  var ps = conn.prepareStatement(CREATE_LINK_TABLE_SQL)) {
+       	 ps.executeUpdate();
+        }
+        catch (Exception e) {
+       	 e.printStackTrace();
+        }
+   }
 
 }
