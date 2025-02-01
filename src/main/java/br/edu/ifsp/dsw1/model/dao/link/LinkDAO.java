dad94a9e1b480de7e85194610316cea02eee37f2
CristianoRDO <crisrooliveira.2004@gmail.com>
2025-02-01 19:11:35 -0300

    Implementação das Querys para Manipulação de Links

--- src/main/java/br/edu/ifsp/dsw1/model/dao/link/LinkDAO.java
+++ src/main/java/br/edu/ifsp/dsw1/model/dao/link/LinkDAO.java
@@ -4,8 +4,10 @@ import br.edu.ifsp.dsw1.model.entity.Link;
 
 public interface LinkDAO {
 	
+	public boolean create(Link link);
+	public boolean delete(Link link);
+	public boolean update(Link link);
 	public Link findyById();
 	public Link findyByUser();
-	public boolean create(Link link);
-
+	
 }
--- src/main/java/br/edu/ifsp/dsw1/model/dao/link/LinkDAOImp.java
+++ src/main/java/br/edu/ifsp/dsw1/model/dao/link/LinkDAOImp.java
@@ -4,12 +4,24 @@ import br.edu.ifsp.dsw1.model.entity.Link;
 
 public class LinkDAOImp implements LinkDAO{
 	
-	private static final String INSERT_LINK_SQL = "";
-	private static final String FIND_BY_ID_SQL = "";
-	private static final String FIND_BY_USER_SQL = "";
+	private static final String INSERT_LINK_SQL = "INSERT INTO link (link_curto, link_original, user) VALUES (?,?,?)";
+	private static final String DELETE_LINK_SQL = "DELETE FROM link WHERE link_curto = ?";
+	private static final String UPDATE_LINK_SQL = "UPDATE link SET link_curto = ? WHERE link_curto = ?";
+	private static final String FIND_BY_ID_SQL = "SELECT link_curto, link_original FROM link WHERE link_curto = ?";
+	private static final String FIND_BY_USER_SQL = "SELECT link_curto, link_original FROM link WHERE user = ?";
 
 	@Override
 	public boolean create(Link link) {
+		return false;
+	}
+	
+	@Override
+	public boolean delete(Link link) {
+		return false;
+	}
+	
+	@Override
+	public boolean update(Link link) {
 		return false;
 	}
 
