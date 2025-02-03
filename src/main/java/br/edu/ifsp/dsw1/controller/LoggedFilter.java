9dab0bbba472257f8f1290f206c1687e1db3a1f8
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-02-03 10:35:31 -0300

    Habilitando os filtros

--- src/main/java/br/edu/ifsp/dsw1/controller/LoggedFilter.java
+++ src/main/java/br/edu/ifsp/dsw1/controller/LoggedFilter.java
@@ -9,34 +9,30 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 
 @WebFilter(urlPatterns = { "/disconnected.do", "/index.jsp", "/login-form.jsp", "/register-form.jsp", } )
 public class LoggedFilter implements Filter {
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-//        var httpRequest = (HttpServletRequest) request;
-//        var session = httpRequest.getSession(false);
-//
-//        // Verificar se a URL tem o parâmetro "action" com valor "contact-page" ou "questions-page"
-//        String action = httpRequest.getParameter("action");
-//
-//        if (action != null && (action.equals("contact-page") || action.equals("questions-page"))) {
-//            // Permite a requisição para as páginas de contacts ou questions
-//            chain.doFilter(request, response);
-//        } else {
-//            // Se a sessão existir e não for uma das páginas permitidas, redireciona para index-logged.jsp
-//            if (session != null && session.getAttribute("user") != null) {
-//                var dispatcher = request.getRequestDispatcher("/logged.do?action=logged-page");
-//                dispatcher.forward(request, response);
-//            } else {
-//                // Se a sessão não existir, permite a requisição normalmente
-//                chain.doFilter(request, response);
-//            }
-//        }
-		chain.doFilter(request, response);
-    }
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        var httpRequest = (HttpServletRequest) request;
+        var session = httpRequest.getSession(false);
 
+        // Verificar se a URL tem o parâmetro "action" com valor "contact-page" ou "questions-page"
+        String action = httpRequest.getParameter("action");
+
+        if (action != null && (action.equals("contact-page") || action.equals("questions-page"))) {
+            // Permite a requisição para as páginas de contacts ou questions
+            chain.doFilter(request, response);
+        } else {
+            // Se a sessão existir e não for uma das páginas permitidas, redireciona para index-logged.jsp
+            if (session != null && session.getAttribute("user") != null) {
+                var dispatcher = request.getRequestDispatcher("/logged.do?action=logged-page");
+                dispatcher.forward(request, response);
+            } else {
+                // Se a sessão não existir, permite a requisição normalmente
+                chain.doFilter(request, response);
+            }
+        }
+    }
 }
